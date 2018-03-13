1b88c5690d3f5cbe28239390a99c71385f5d9fe4
Emad Aghayi <emad.aghayi@gmail.com>
2018-03-13 19:33:48 -0400

    Initial commit

---
+++ src/main/java/com/symplicity/config/core/SpringMvcInitializer.java
@@ -0,0 +1,24 @@
+package com.symplicity.config.core;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.symplicity.config.AppConfig;
+
+public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+	
+}
---
+++ src/main/java/com/symplicity/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.symplicity.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
